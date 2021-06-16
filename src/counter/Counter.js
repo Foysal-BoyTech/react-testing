3082fe6804506afc83acee1db57bb20c7e0b3e85
Foysal Mohammad <foysal@earlblue.com>
2021-06-16 23:44:07 +0100

    few tests written for counter component

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Counter() {
+  const [counterValue, setCounterValue] = useState(0)
   return (
     <div>
       <h2 data-testid="header">My Counter</h2>
+      <h2 data-testid="counter">{counterValue}</h2>
     </div>
   )
 }
--- src/counter/__test__/Counter.test.js
+++ src/counter/__test__/Counter.test.js
@@ -8,3 +8,26 @@ test('header renders with correct text', () => {
   const headerEl = getByTestId('header')
   expect(headerEl.textContent).toBe('My Counter')
 })
+
+test('counter initially start with text of 0', () => {
+  const { getByTestId } = render(<Counter />)
+  const counterEl = getByTestId('counter')
+  expect(counterEl.textContent).toBe('0')
+})
+
+test('input contains initial value of 1', () => {
+  const { getByTestId } = render(<Counter />)
+  const inputEl = getByTestId('input')
+  expect(inputEl.value).toBe('1')
+})
+
+test('add button renders wtih +', () => {
+  const { getByTestId } = render(<Counter />)
+  const addBtn = getByTestId('add-btn')
+  expect(addBtn.textContent).toBe('+')
+})
+test('substract button renders wtih -', () => {
+  const { getByTestId } = render(<Counter />)
+  const substractBtn = getByTestId('substract-btn')
+  expect(substractBtn.textContent).toBe('-')
+})
