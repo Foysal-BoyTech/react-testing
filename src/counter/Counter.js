2ccdfd69b6d4bf8fff07e9c647cde3e5511b5dba
Foysal Mohammad <foysal@earlblue.com>
2021-06-17 11:41:31 +0100

    five test passes, input and btn added

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react'
+import './Counter.css'
 
 export default function Counter() {
   const [counterValue, setCounterValue] = useState(0)
+  const [inputValue, setInputValue] = useState(1)
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value)
+  }
   return (
     <div>
       <h2 data-testid="header">My Counter</h2>
       <h2 data-testid="counter">{counterValue}</h2>
+      <button data-testid="substract-btn">-</button>
+      <input
+        data-testid="input"
+        type="number"
+        value={inputValue}
+        name="input"
+        onChange={handleChange}
+        className="text-center"
+      />
+      <button data-testid="add-btn">+</button>
     </div>
   )
 }
